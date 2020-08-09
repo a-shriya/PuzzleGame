f3a098604b26c87d73f962e99aa7fd6ec05004d8
Srinidhi Prasad <srinidhiprasad05@gmail.com>
2020-08-09 09:38:51 +0530

    Made multiple grid system and added a panel for both buttons

--- src/com/example/Board.java
+++ src/com/example/Board.java
@@ -10,55 +10,82 @@ import java.util.TimerTask;
 public class Board extends JFrame
 {
     static int numberOfMoves = 0;
-    JButton[][] squares = new JButton[4][4];
+    private int dimension;
+    static int count;
+    JButton[][] squares = new JButton[10][10];
     JButton timer1 = new JButton();
     JButton moves = new JButton();
 
-    public Board()
+    public Board(int dimension)
     {
-        Countdown countdown = new Countdown();
-        countdown.startCountdown();
-        Container container = getContentPane();
-        container.setLayout((new GridLayout(5, 4)));
 
-        container.add(new Label("Time"));
-        container.add(timer1);
-        container.add(new Label("Number of moves"));
-        container.add(moves);
-        for (int i = 0; i < 4; i++)
+        this.dimension = dimension;
+
+        JPanel jPanel2 = new JPanel();
+
+        Board.Countdown countdown = new Board.Countdown();
+        countdown.startCountdown();
+        jPanel2.setLayout((new GridLayout(dimension, dimension)));
+
+
+        Font bigFont = new Font("serif", Font.BOLD, 16);
+        Font smallFont = new Font("serif", Font.BOLD, 14);
+
+        JPanel jPanel = new JPanel();
+        Label label1 = new Label("Time   :");
+        label1.setFont(smallFont);
+        Label label2 = new Label("   Number of moves :");
+        label2.setFont(smallFont);
+        jPanel.add(label1);
+        moves.setText("0");
+        timer1.setFont(bigFont);
+        moves.setFont(bigFont);
+        jPanel.add(timer1);
+        jPanel.add(label2);
+        jPanel.add(moves);
+
+
+        for (int i = 0; i < dimension; i++)
         {
-            for (int j = 0; j < 4; j++)
+            for (int j = 0; j < dimension; j++)
             {
                 JButton num = new JButton();
                 squares[i][j] = num;
-                container.add(num);
+                jPanel2.add(num);
             }
         }
 
-        ButtonListener pushed = new ButtonListener();
+        Board.ButtonListener pushed = new Board.ButtonListener();
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < dimension; i++)
         {
-            for (int j = 0; j < 4; j++)
+            for (int j = 0; j < dimension; j++)
             {
                 squares[i][j].addActionListener(pushed);
             }
         }
+
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(BorderLayout.NORTH, jPanel);
+        frame.getContentPane().add(BorderLayout.CENTER, jPanel2);
+        frame.setSize(400,400);
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
     public void scramble()
     {
-        boolean[] used = new boolean[16];
+        boolean[] used = new boolean[dimension*dimension];
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < dimension; i++)
         {
-            for (int j = 0; j < 4; j++)
+            for (int j = 0; j < dimension; j++)
             {
-                int val = (int) (16 * Math.random());
+                int val = (int) (dimension*dimension * Math.random());
 
                 while (used[val])
                 {
-                    val = (int) (16 * Math.random());
+                    val = (int) (dimension*dimension * Math.random());
                 }
 
                 used[val] = true;
@@ -82,55 +109,57 @@ public class Board extends JFrame
     {
         JButton empty = null;
 
-            if (i < 3 && squares[i + 1][j].getBackground() == Color.blue)
-                empty = squares[i + 1][j];
-            if (i > 0 && squares[i - 1][j].getBackground() == Color.blue)
-                empty = squares[i - 1][j];
-            if (j < 3 && squares[i][j + 1].getBackground() == Color.blue)
-                empty = squares[i][j + 1];
-            if (j > 0 && squares[i][j - 1].getBackground() == Color.blue)
-                empty = squares[i][j - 1];
+        if (i < dimension-1 && squares[i + 1][j].getBackground() == Color.blue)
+            empty = squares[i + 1][j];
+        if (i > 0 && squares[i - 1][j].getBackground() == Color.blue)
+            empty = squares[i - 1][j];
+        if (j < dimension-1 && squares[i][j + 1].getBackground() == Color.blue)
+            empty = squares[i][j + 1];
+        if (j > 0 && squares[i][j - 1].getBackground() == Color.blue)
+            empty = squares[i][j - 1];
 
-            if (empty == null)
-            {
-                return;
-            }
-            else
-            {
-                empty.setText(squares[i][j].getText());
-                empty.setBackground(Color.lightGray);
-                squares[i][j].setText("");
-                squares[i][j].setBackground(Color.blue);
-            }
-            numberOfMoves++;
-            moves.setText(String.valueOf(numberOfMoves));
+        if (empty == null)
+        {
+            return;
+        }
+        else
+        {
+            empty.setText(squares[i][j].getText());
+            empty.setBackground(Color.lightGray);
+            squares[i][j].setText("");
+            squares[i][j].setBackground(Color.blue);
+        }
+        numberOfMoves++;
+        moves.setText(String.valueOf(numberOfMoves));
     }
 
 
-//    void printResult()
-//    {
-//        while(isGameOver()) {
-//            System.out.println("You Won!!!!!!!!!");
-//        }
-//    }
+    void printResult()
+    {
+        if(count == dimension-1) {
+            System.out.println("You Won!!!!!!!!!");
+        }
+    }
 
 
     boolean isGameOver()
     {
         boolean checkGameOver = true;
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (squares[i][j].getText().equals((i * 4 + j + 1) + ""))
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                if (squares[i][j].getText().equals(String.valueOf((i * dimension) + j + 1)) && squares[dimension][dimension].getBackground() == Color.blue)
                 {
                     checkGameOver = true;
-                }
-                else
-                    {
+                    count ++;
+                    System.out.println("Won");
+                } else {
                     checkGameOver = false;
-                    break;
                 }
             }
+        }
+        if(count == dimension-1) {
+            System.out.println("You Won!!!!!!!!!");
         }
 
         return checkGameOver;
@@ -145,9 +174,9 @@ public class Board extends JFrame
             Object square = e.getSource();
 
             outer:
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < dimension; i++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < dimension; j++)
                 {
                     if (squares[i][j] == square)
                     {
@@ -171,7 +200,7 @@ public class Board extends JFrame
 
                 public void run()
                 {
-                   timer1.setText(++i + "");
+                    timer1.setText(++i + "");
                 }
             }, 0, 1000);
         }
--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,22 +1,11 @@
 package com.example;
-import javax.swing.*;
 
 public class Main
 {
     public static void main(String[] args)
     {
 
-//        Board gameBoard = new Board();
-//        gameBoard.setTitle ("ARRANGE PUZZLE GAME");
-//        gameBoard.setSize( 400,  400);
-//        gameBoard.setVisible( true);
-//        gameBoard.scramble ();
-//        gameBoard.printResult();
-//        gameBoard.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
-
-        DynamicBoard dynamicBoard = new DynamicBoard(3);
-        dynamicBoard.setTitle ("ARRANGE PUZZLE GAME");
-        dynamicBoard.scramble ();
-        dynamicBoard.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+        Board gameBoard = new Board(3);
+        gameBoard.scramble ();
     }
 }
